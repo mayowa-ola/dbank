4026c9be9a8e7aa6e8be94a035bd87558254557c
Mayowa Oladunjoye <mayowaoladunjoye@gmail.com>
2022-07-12 18:26:23 +0100

    dbank done

--- src/dbank/main.mo
+++ src/dbank/main.mo
@@ -1,5 +1,47 @@
-actor {
-  public func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+import Debug "mo:base/Debug";
+import Time "mo:base/Time";
+import Float "mo:base/Float";
+
+actor DBank{
+  stable var currentValue:Float = 300; //var is used to create a variable, this can be changed
+  //stable persist the variable so that is is stored 
+  // currentValue :=300; //replace current value to 100, assignment in mokoto is ":="
+
+  stable var startTime = Time.now();
+  Debug.print(debug_show(startTime));
+  let id = 278877974; //let is similar to const in js, once set, it shouldn't be changed
+  // id :=
+
+  // Debug.print(debug_show(currentValue))
+  // Debug.print(debug_show(id));
+
+  public func topUp(amount: Float) { //func is used to create functions - Nat is any natural number that is positive
+    currentValue += amount;
+
+    Debug.print(debug_show(currentValue));
   };
-};
+
+  // topUp();
+
+  public func withdraw(amount: Float) { 
+    let tempValue:Float = currentValue - amount;
+    if(tempValue >= 0){
+      currentValue -= amount;
+      Debug.print(debug_show(currentValue));
+    } else{
+      Debug.print("Amount too large to be withdrawn");
+    }
+  };
+
+  public query func checkBalance(): async Float{
+    return currentValue;
+  }; 
+
+  public func compound(){
+    let currentTime = Time.now();
+    let timeElapsedNS = currentTime - startTime; 
+    let timeElapsedS = timeElapsedNS / 1000000000;
+    currentValue := currentValue * (1.01 ** Float.fromInt(timeElapsedS));
+    startTime := currentTime;
+  }
+}
--- src/dbank_assets/src/index.js
+++ src/dbank_assets/src/index.js
@@ -1,19 +1,35 @@
-import { dbank } from "../../declarations/dbank";
+import {dbank} from "../../declarations/dbank"
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
-
-  const name = document.getElementById("name").value.toString();
-
-  button.setAttribute("disabled", true);
-
-  // Interact with foo actor, calling the greet method
-  const greeting = await dbank.greet(name);
-
-  button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
-  return false;
+window.addEventListener("load", async function() {
+  const currentAmount = await dbank.checkBalance();
+  document.getElementById("value").innerText = Math.round(currentAmount * 100) / 100;
+  // console.log("window loaded")
 });
+
+document.querySelector("form").addEventListener("submit", async function(event){
+  event.preventDefault();
+
+  const button = event.target.querySelector("#submit-btn")
+
+  const inputAmount = document.getElementById("input-amount").value;
+  const outputAmount = document.getElementById("withdrawal-amount").value;
+
+  button.setAttribute("disabled", true)
+
+  if(inputAmount.length != 0){
+    await dbank.topUp(parseFloat(inputAmount))
+  }
+  if(outputAmount.length != 0){
+    await dbank.withdraw(parseFloat(outputAmount))
+  }
+
+    await dbank.compound();
+
+  const currentAmount = await dbank.checkBalance();
+
+  document.getElementById("value").innerText = Math.round(currentAmount * 100) / 100;
+
+  document.getElementById("input-amount").value = "";
+  document.getElementById("withdrawal-amount").value = ""
+  button.removeAttribute("disabled")
+})
